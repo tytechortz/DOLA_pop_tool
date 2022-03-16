31968a52d6622acae9b72cb3776e36f7610b12de
James Swank <jswank@tchd.org>
2022-03-16 15:40:47 -0600

    adds css

--- app.py
+++ app.py
@@ -1,2 +1,22 @@
 from dash import Dash, html, dcc
 import pandas as pd
+from datetime import date
+
+
+
+def get_layout():
+    return html.Div(
+        [
+            html.Div([
+                html.H4('DOLA Pop')
+            ])
+        ]
+    )
+
+app = dash.Dash(__name__)
+app.layout = get_layout
+app.config['suppress_callback_exceptions']=True
+
+
+if __name__=="__main__":
+    app.run_server(debug=True)
